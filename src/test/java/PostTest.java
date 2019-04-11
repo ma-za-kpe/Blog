59369287e943dc040910828291899a568f1a18d8
ma-za-kpe <makpalyy@gmail.com>
2019-04-11 09:26:11 +0200

    add custom css and remove other repeated index route

--- src/test/java/PostTest.java
+++ src/test/java/PostTest.java
@@ -1,3 +1,4 @@
+import org.junit.After;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,5 +8,32 @@ public class PostTest {
     public void NewPostObjectGetsCorrectlyCreated_true() throws Exception {
         Post post = new Post("Day 1: Intro");
         assertEquals(true, post instanceof Post);
+    }
+
+    @Test
+    public void PostInstantiatesWithContent_true() throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals("Day 1: Intro", post.getContent());
+
+    }
+
+    @After
+    public void tearDown() {
+        Post.clearAllPosts(); //clear out allll the posts before each test.
+    }
+
+    @Test
+    public void AllPostsAreCorrectlyReturned_true() {
+        Post post = new Post("Day 1: Intro");
+        Post otherPost = new Post ("How to pair successfully");
+        assertEquals(2, Post.getAll().size());
+    }
+
+    @Test
+    public void AllPostsContainsAllPosts_true() {
+        Post post = new Post("Day 1: Intro");
+        Post otherPost = new Post ("How to pair successfully");
+        assertTrue(Post.getAll().contains(post));
+        assertTrue(Post.getAll().contains(otherPost));
     }
 }
